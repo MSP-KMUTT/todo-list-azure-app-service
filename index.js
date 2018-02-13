0d3e1080d49ec61200b5beea1e75af5e5ad41310
Jirachai Chansivanon <antronic.inc@gmail.com>
2018-02-13 21:11:49 +0700

    [UPDATE] Edit response text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { createServer } = require('http')
 
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello KMUTT!');
+  res.end('Hello from the another side!');
 })
 
 const port = process.env.PORT || 1337;
